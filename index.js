ba71c57448eb1693be7704141ddfe07ea04a6e7d
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-14 20:28:11 +0200

    created numbers for each square in the grid

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 //prendo gridDom
 const gridDom = document.getElementById ('grid')
 
+//creo 100 square con ciclo for:
 
-
-//creo 100 square con ciclo for
-
-
-for (let i = 0; i < 100 ; i++) {
-    
-    const currentElement = createGridSquare ();
+for (let i = 1; i <= 100 ; i++) {
    
+
+    //richiamo la funzione che mi crea grid con square    
+    const currentElement = createGridSquare (i);
+    //aggiungo event listner per click   
     currentElement.addEventListener ('click',
     function () {
             this.classList.toggle ('clicked');
         }
     );
         gridDom.append (currentElement); 
-
 } 
 
 
 
 
-function createGridSquare () {
+
+
+
+
+
+
+//sezione funzioni:
+
+//1. funzione che mi crea il grid con square
+function createGridSquare (numero) {
 
     const currentElement = document.createElement ('div');
     currentElement.classList.add ('square');
+    
+    currentElement.append(numero)
 
-return currentElement
+    return currentElement
 }
+
+//3. funzione per non ripetere le bombe
+
+
+
 
 
 
