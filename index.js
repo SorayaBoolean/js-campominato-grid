1f6464f114cd5466b91255d3f59650c80c2580f3
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-14 13:41:03 +0200

    refactoring first loop for now i proceed by creating a function for create grid and clicked event

--- index.js
+++ index.js
@@ -7,24 +7,19 @@ const gridDom = document.getElementById ('grid')
 
 
 for (let i = 0; i < 100 ; i++) {
-    //1. Primo metodo
-    //  gridDom.innerHTML += '<div class=square></div>'
-    //2. oppure Secondo metodo
+    
     const currentElement = document.createElement ('div');
     currentElement.classList.add ('square');
-    gridDom.append (currentElement);
-//aggiungo azione al click: colorami square 
+   
+
     currentElement.addEventListener ('click',
     function () {
-        //per rimuovere la classe clicked al secondo click
-        if (this.classList.contains ('clicked')) {
-            this.classList.remove ('clicked')
-            } else {
-        this.classList.add ('clicked')
-            };
+            this.classList.toggle ('clicked');
         }
-    )
-} //con this.classList.toggle ('clicked') esegue remove e add
+    );
+        gridDom.append (currentElement); 
+
+} 
 
 
 
